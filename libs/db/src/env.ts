f6eedce97e88041a12010ad69e0186f68ac10789
Clinton Bess <clintonbess3@gmail.com>
2025-10-14 21:58:27 -0700

    build build

--- libs/db/src/env.ts
+++ libs/db/src/env.ts
@@ -1,8 +1,5 @@
 import dotenv from "dotenv";
-import dotenvExpand from "dotenv-expand";
 import path from "node:path";
 
 const envPath = process.env.NODE_ENV === "test" ? ".env.test" : ".env";
-dotenvExpand.expand(
-  dotenv.config({ path: path.resolve(envPath), override: true })
-);
+dotenv.config({ path: path.resolve(envPath), override: true });
