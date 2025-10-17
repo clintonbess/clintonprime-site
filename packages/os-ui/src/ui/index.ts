cc9633809e806032e8a2026e3051df2e30fbe7c5
Clinton Bess <clintonbess3@gmail.com>
2025-10-17 15:34:56 -0700

    build updates

--- packages/os-ui/src/ui/index.ts
+++ packages/os-ui/src/ui/index.ts
@@ -1,3 +1,3 @@
-// packages/os-ui/src/index.ts
-export * from "./ui/prime-window";
-export * from "./ui/prime-tabs-window";
+// Public UI exports (local to ./ui)
+export * from "./prime-window";
+export * from "./prime-tabs-window";
