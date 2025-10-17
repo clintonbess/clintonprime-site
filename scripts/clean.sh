053f0865048d2d6d3b9a3eca4038ce0a62abf789
Clinton Bess <clintonbess3@gmail.com>
2025-10-16 20:03:31 -0700

    updates types build

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
 pnpm dlx rimraf **/node_modules apps/**/node_modules libs/**/node_modules packages/**/node_modules
 pnpm dlx rimraf apps/**/dist libs/**/dist packages/**/dist
 
- pnpm install --no-frozen-lockfile
+pnpm install --no-frozen-lockfile
